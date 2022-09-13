642a7c1f9c39a299c7f1bb7021f27f55bc0edd70
yajit94 <112825638+yajit94@users.noreply.github.com>
2022-09-13 20:25:17 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,6 +4,6 @@ import boto3
 def lambda_handler(event, context):
   client = boto3.client('s3')
   response = client.create_bucket(
-      Bucket='mybucket-forvita',
+      Bucket='mybucket-forvita123',
       )
   
